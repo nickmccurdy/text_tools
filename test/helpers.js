f847fa037123db264c0a69dde9f8683e3c2603b1
Nicolas McCurdy <thenickperson@gmail.com>
2014-05-14 17:46:32 -0400

    Add tests for Helpers

---
+++ test/helpers.js
@@ -0,0 +1,41 @@
+"use strict";
+
+describe("Helpers", function () {
+  describe(".modifyAsList", function () {
+    it("adds text to the beginning of each line of a string", function () {
+      var callback = function (list) {
+        return list.map(function (line) {
+          return "- " + line;
+        });
+      };
+      expect(Helpers.modifyAsList("one\ntwo\nthree", callback))
+            .to.be("- one\n- two\n- three");
+    });
+
+    it("sorts lines in a string", function () {
+      var callback = function (list) {
+        return list.sort();
+      };
+      expect(Helpers.modifyAsList("2\n1\n3", callback)).to.be("1\n2\n3");
+    });
+  });
+
+  describe(".htmlCaseChanger", function () {
+    it("uppercases a <strong> tag", function () {
+      expect(Helpers.htmlCaseChanger("<strong>hello</strong>", "upper"))
+            .to.be("<STRONG>hello</STRONG>");
+    });
+
+    it("lowercases a <STRONG> tag", function () {
+      expect(Helpers.htmlCaseChanger("<STRONG>HELLO</STRONG>", "lower"))
+            .to.be("<strong>HELLO</strong>");
+    });
+  });
+
+  describe(".toTitleCase", function () {
+    it("converts a mostly lowercased string to title case", function () {
+      expect(Helpers.toTitleCase("the power of JavaScript"))
+            .to.be("The Power of JavaScript");
+    });
+  });
+});
