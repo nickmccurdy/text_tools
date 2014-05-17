fd4361723e9d4ae4a1e4524770f96f09c4d4a6af
Nicolas McCurdy <thenickperson@gmail.com>
2014-05-17 16:30:22 -0400

    Add tests for $#swapClasses()

--- test/ui.js
+++ test/ui.js
@@ -47,3 +47,48 @@ describe("UI", function () {
     });
   });
 });
+
+describe("$", function () {
+  var div;
+
+  beforeEach(function () {
+    div = $(document.createElement("div"));
+  });
+
+  describe("#swapClasses", function () {
+    context("when the element has the first of the two classes", function () {
+      beforeEach(function () {
+        div.addClass("blue");
+      });
+
+      it("removes the first class and adds the second class", function () {
+        div.swapClasses("blue", "red");
+
+        expect(div.hasClass("blue")).to.be(false);
+        expect(div.hasClass("red")).to.be(true);
+      });
+    });
+
+    context("when the element has the second of the two classes", function () {
+      beforeEach(function () {
+        div.addClass("red");
+      });
+
+      it("removes the second class and adds the first class", function () {
+        div.swapClasses("blue", "red");
+
+        expect(div.hasClass("blue")).to.be(true);
+        expect(div.hasClass("red")).to.be(false);
+      });
+    });
+
+    context("when the element has neither of the two classes", function () {
+      it("adds the first class", function () {
+        div.swapClasses("blue", "red");
+
+        expect(div.hasClass("blue")).to.be(true);
+        expect(div.hasClass("red")).to.be(false);
+      });
+    });
+  });
+});
