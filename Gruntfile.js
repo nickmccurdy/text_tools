54d7f9b5c50f9db15aa8328a37c0cf770cbe766d
Nicolas McCurdy <thenickperson@gmail.com>
2014-05-24 19:52:21 -0400

    Switch from jslint to jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,28 @@
 module.exports = function (grunt) {
   grunt.loadNpmTasks("grunt-contrib-csslint");
-  grunt.loadNpmTasks("grunt-jslint");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
 
   grunt.initConfig({
-    jslint: {
-      build: {
-        src: "js/*.js",
-        directives: {
-          predef: ["$", "_"],
-          indent: 2,
-          node: true,
-          nomen: true,
-          regexp: true
-        }
+   jshint: {
+      files: "js/*.js",
+      options: {
+        globals: {
+          $: false,
+          _: false
+        },
+        indent: 2,
+        node: true,
+        nomen: true,
+        regexp: true
       }
     },
     csslint: {
-      src: "*.css",
+      files: "*.css",
       options: {
         "adjoining-classes": false
       }
     }
   });
 
-  grunt.registerTask("default", ["jslint", "csslint"]);
+  grunt.registerTask("default", ["jshint", "csslint"]);
 };
