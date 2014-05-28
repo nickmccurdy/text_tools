49df24f0920a44de63f9d3cd877c38d74883dc4c
Nicolas McCurdy <thenickperson@gmail.com>
2014-05-27 21:40:13 -0400

    Use a .jshintrc

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,36 +5,7 @@ module.exports = function (grunt) {
   grunt.initConfig({
     jshint: {
       files: ['js/*.js', 'test/*.js', '!test/mocha.js'],
-      options: {
-        browser: true,
-        globalstrict: true,
-        jquery: true,
-        quotmark: 'single',
-        globals: {
-          Effects: true,
-          Elements: true,
-          Helpers: true,
-          Panels: true,
-          View: true,
-
-          // libraries
-          _: false,
-          Cookies: false,
-
-          // mocha
-          describe: false,
-          context: false,
-          it: false,
-          before: false,
-          beforeEach: false,
-          after: false,
-          afterEach: false,
-
-          // other test libraries
-          expect: false,
-          sinon: false
-        }
-      }
+      options: { jshintrc: true }
     },
     csslint: {
       files: '*.css'
